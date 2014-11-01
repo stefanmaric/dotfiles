9b33f13800e356a4775bde02648ab50ad80468b8
Stefan Maric <alexstefan92@gmail.com>
2014-11-01 10:42:08 -0430

    Improving add_padding. Now it can add padding to each side indenpendently

--- fish/functions/add_padding.fish
+++ fish/functions/add_padding.fish
@@ -1,11 +1,34 @@
-function add_padding
-  set -l padded $argv[1]
-  if test (count $argv) -lt 2
-    set padded "    $padded    "
-  else
-    for i in (seq $argv[2])
-      set padded " $padded "
-    end
+# add_padding string
+# add_padding string padding_for_both_sides
+# add_padding string padding_left padding_right
+
+function add_padding --description "Add padding to a string"
+
+  set -l string $argv[1]
+
+  switch (count $argv) > /dev/null
+    case 1
+
+      set string " $string "
+
+    case 2
+
+	    for i in (seq $argv[2])
+	      set string " $string "
+	    end
+
+	  case 3
+
+	    for i in (seq $argv[2])
+	      set string " $string"
+	    end
+
+	    for i in (seq $argv[3])
+	      set string "$string "
+	    end
+
   end
-  echo "$padded"
+
+  echo "$string"
+
 end

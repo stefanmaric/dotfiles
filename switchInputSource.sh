522728db57e910c8a3cf24b96b7e678fcf3d618c
Stefan Maric <alexstefan92@gmail.com>
2014-11-05 13:19:35 -0430

    Better solution for input source swtiching workaround. Works with any number of input sources.

--- switchInputSource.sh
+++ switchInputSource.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-CURRENT=$(gsettings get org.gnome.desktop.input-sources sources)
-US="[('xkb', 'us')]"
+# Workaround for gnome issue with iBus and input source switching.
+# Uses gesettings to retrieve current configuration and uses sed to shift input sources, i.e.: change to next input source.
+# Works with any input-source settings.
+# You can bind a keyboard shortcut to this script.
+#
+# More info:
+#
+# * https://bugs.launchpad.net/ubuntu/+source/gnome-settings-daemon/+bug/1244158
+# * https://bugzilla.gnome.org/show_bug.cgi?id=711001
 
-if [ "$CURRENT" == "$US" ]; then
-	gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'latam')]"
-else
-	gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us')]"
-fi
+gsettings set org.gnome.desktop.input-sources sources "$(gsettings get org.gnome.desktop.input-sources sources | sed -e 's/^\[\(([^)]*)\)\(.*\)\(([^)]*)\)\]$/[\3, \1\2]/g' -e 's/[, ]*\(\]$\)/\1/g')"

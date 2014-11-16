7c069488293fb6a6d735a2c018f9769e4acc23f4
Stefan Maric <alexstefan92@gmail.com>
2014-11-16 18:02:49 -0430

    slpitOpenVpnFiles: more comments.

--- fish/functions/splitOpenVpnFile.fish
+++ fish/functions/splitOpenVpnFile.fish
@@ -11,7 +11,7 @@
 # 
 # More info: http://askubuntu.com/questions/450493/openvpn-cant-import-configurations-on-new-14-04-installation
 
-function splitOpenVpnFile --description "Split OpenVPN config files and save it in a given dir"
+function splitOpenVpnFile --description "Split OpenVPN config files and save them in the given dir."
 	set -l dirPath
 
 	if [ (count $argv) -gt 1 ]
@@ -22,16 +22,20 @@ function splitOpenVpnFile --description "Split OpenVPN config files and save it 
 
 	mkdir -p $dirPath
 
+	# Extract keys and put them into the destination dir
 	cat $argv[1]  | sed -n '/<ca>/,/<\/ca>/{/<ca>/b;/<\/ca>/b;p}' >  $dirPath/ca.crt
 	cat $argv[1]  | sed -n '/<cert>/,/<\/cert>/{/<cert>/b;/<\/cert>/b;p}' >  $dirPath/cert.crt
 	cat $argv[1]  | sed -n '/<key>/,/<\/key>/{/<key>/b;/<\/key>/b;p}' >  $dirPath/key.key
 
+	# Copy the source config file...
 	cp $argv[1] $dirPath/conf.ovpn
 
+	# then remove the keys...
 	sed -ni '/<cert>/{:a;N;/<\/cert>/!ba;N;s/.*\n//};p' $dirPath/conf.ovpn
 	sed -ni '/<ca>/{:a;N;/<\/ca>/!ba;N;s/.*\n//};p' $dirPath/conf.ovpn
 	sed -ni '/<key>/{:a;N;/<\/key>/!ba;N;s/.*\n//};p' $dirPath/conf.ovpn
 
+	# add finally add a references to the previously generated key and crt files.
 	echo "ca ca.crt" >>  $dirPath/conf.ovpn
 	echo "cert cert.crt" >>  $dirPath/conf.ovpn
 	echo "key key.key" >>  $dirPath/conf.ovpn

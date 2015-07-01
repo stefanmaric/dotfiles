586769d4725974653d6263cb3227b26d9f674c53
Stefan Maric <alexstefan92@gmail.com>
2015-07-01 01:53:20 -0430

    Preserve hh:mm:ss accuracy on sort

--- fish/functions/mycommits.fish
+++ fish/functions/mycommits.fish
@@ -1,3 +1,3 @@
 function mycommits --description 'Recursively search for git repos, aggregate all commits made by me since given date and sort them by commit date.'
-	find . -name ".git" -type d -exec git -C '{}' log --all --no-merges --since="$argv[1]" --committer="stefan" --pretty=format:"%cd {}: %s" --date=short \; -exec echo "" \; | sed 's/\.\///g' | sed 's/\/\.git//g' | grep -v '^$' | sort
+	find . -name ".git" -type d -exec git -C '{}' log --all --no-merges --since="$argv[1]" --committer="stefan" --pretty=format:"%ci {}: %s" \; -exec echo "" \; | grep -v '^$' | sort | sed 's/\.\///g' | sed 's/\/\.git//g' | sed 's/\([0-9]\{2\}[: ]\)\{3\}-\?[0-9]\{4\}//g'
 end

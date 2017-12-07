ccc6b36f4eecd3cefc479efd76e20e7d490dda96
Stefan Maric <me@stefanmaric.com>
2017-12-07 13:59:35 +0100

    Tweak mycommits func to show basedir of repos

--- .config/fish/functions/mycommits.fish
+++ .config/fish/functions/mycommits.fish
@@ -1,3 +1,9 @@
 function mycommits --description 'Recursively search for git repos, aggregate all commits made by me since given date and sort them by commit date.'
-	find . -name ".git" -type d -exec git -C '{}' log --all --no-merges --since="$argv[1]" --committer="stefan" --pretty=format:"%ci {}: %s" \; -exec echo "" \; | grep -v '^$' | sort | sed 's/\.\///g' | sed 's/\/\.git//g' | sed 's/\([0-9]\{2\}[: ]\)\{3\}-\?[0-9]\{4\}//g'
+	find . -name ".git" -type d -exec git -C '{}' log --all --no-merges --since="$argv[1]" --committer="stefan" --pretty=format:"%ci, "(basename '{}')", \"%s\"" \; -exec echo "" \; \
+		| grep -v '^$' \
+		| sort \
+		| sed 's/\.\///g' \
+		| sed 's/\/\.git//g' \
+		| sed 's/\.git, //g' \
+		| sed 's/ \([0-9]\{2\}[: ]\)\{3\}-\?[0-9]\{4\}//g'
 end

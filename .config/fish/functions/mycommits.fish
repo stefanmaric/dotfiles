5d915e43f98cd5968cd5972bccb9c50dc7ca8dc9
Stefan Maric <me@stefanmaric.com>
2018-01-15 13:47:27 +0100

    Use author date instead of commit date for mycommits script

--- .config/fish/functions/mycommits.fish
+++ .config/fish/functions/mycommits.fish
@@ -1,5 +1,5 @@
 function mycommits --description 'Recursively search for git repos, aggregate all commits made by me since given date and sort them by commit date.'
-	find . -name ".git" -type d -exec git -C '{}' log --all --no-merges --since="$argv[1]" --committer="stefan" --pretty=format:"%ci, "(basename '{}')", \"%s\"" \; -exec echo "" \; \
+	find . -name ".git" -type d -exec git -C '{}' log --all --no-merges --since="$argv[1]" --committer="stefan" --pretty=format:"%ai, "(basename '{}')", \"%s\"" \; -exec echo "" \; \
 		| grep -v '^$' \
 		| sort \
 		| sed 's/\.\///g' \

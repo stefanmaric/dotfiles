790047aed13d7864acb74d157076d2a3bad3775c
Stefan Maric <me@stefanmaric.com>
2020-04-24 16:10:39 +0200

    Cleanup fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,5 +4,3 @@ set -gx N_PREFIX $HOME/n
 set -gx PATH $PATH $N_PREFIX/bin
 
 set -gx fish_prompt_pwd_dir_length 3
-
-set -gx GOPATH $HOME/go; set -gx GOROOT $HOME/.go; set -gx PATH $GOPATH/bin $PATH; # g-install: do NOT edit, see https://github.com/stefanmaric/g

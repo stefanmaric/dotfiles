75fe8602c0ce504beea9f6a6b312c8c74484134e
Stefan Maric <me@stefanmaric.com>
2022-03-21 13:22:27 +0100

    Integrate myCommits and format-records into a single script

--- fish/functions/mycommits.fish
+++
@@ -1,9 +0,0 @@
-function mycommits --description 'Recursively search for git repos, aggregate all commits made by me since given date and sort them by commit date.'
-	find . -name ".git" -type d -exec git -C '{}' log --all --no-merges --since="$argv[1]" --committer="stefan" --pretty=format:"%ai, "(basename '{}')", \"%s\"" \; -exec echo "" \; \
-		| grep -v '^$' \
-		| sort \
-		| sed 's/\.\///g' \
-		| sed 's/\/\.git//g' \
-		| sed 's/\.git, //g' \
-		| sed 's/ \([0-9]\{2\}[: ]\)\{3\}[+-]\?[0-9]\{4\}//g'
-end

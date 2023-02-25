7d1a221f41a1998a19837488e85edd2c035dfcd1
Stefan Maric <me@stefanmaric.com>
2023-02-25 11:49:04 +0100

    Tweak cloneall util to skip forks

--- fish/functions/cloneall.fish
+++ fish/functions/cloneall.fish
@@ -1,7 +1,7 @@
 # Requires Github's official CLI client: gh
 
 function cloneall --description 'Script to clone all user or org repos from Github' --argument namespace
-    for repo in (gh repo list $namespace --limit 1000 | awk '{print $1}')
+    for repo in (gh repo list $namespace  --source --limit 1000 | awk '{print $1}')
         gh repo clone "$repo" (echo $repo | cut -d '/' -f 2)
     end
 end

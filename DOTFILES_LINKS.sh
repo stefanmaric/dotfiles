fcd4564a925d4c5840935ff3ae7aed1de3b032aa
Stefan Maric <me@stefanmaric.com>
2026-07-09 13:25:29 +0200

    Normalize bin to ~/.local/bin and make it priority

--- DOTFILES_LINKS.sh
+++ DOTFILES_LINKS.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 
 declare -A DOTFILES_LINKS=(
-  [bin/git-gone-stats]="$HOME/bin/git-gone-stats"
-  [bin/git-env-only-branches]="$HOME/bin/git-env-only-branches"
-  [bin/git-purge]="$HOME/bin/git-purge"
-  [bin/git-publish]="$HOME/bin/git-publish"
-  [bin/worklog]="$HOME/bin/worklog"
+  [bin/git-gone-stats]="$HOME/.local/bin/git-gone-stats"
+  [bin/git-env-only-branches]="$HOME/.local/bin/git-env-only-branches"
+  [bin/git-purge]="$HOME/.local/bin/git-purge"
+  [bin/git-publish]="$HOME/.local/bin/git-publish"
+  [bin/worklog]="$HOME/.local/bin/worklog"
   [fish/conf.d/history-expansion.fish]="$HOME/.config/fish/conf.d/history-expansion.fish"
   [fish/conf.d/starship-context.fish]="$HOME/.config/fish/conf.d/starship-context.fish"
   [fish/config.fish]="$HOME/.config/fish/config.fish"
--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,12 @@
-set -gx PATH $PATH $HOME/bin
-set -gx PATH $PATH $HOME/.local/bin
-
 atuin init fish | source
 fzf --fish | source
 zoxide init fish | source
 starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+fish_add_path --path "$PNPM_HOME/bin"
+# pnpm end
+
+fish_add_path --move --path "$HOME/.local/bin"
+

f9a99cf607bc5aef6357fd5e5ae6940fff00c7f4
Stefan Maric <me@stefanmaric.com>
2026-07-10 16:41:53 +0200

    Generalize fish config

--- fish/config.fish
+++ fish/config.fish
@@ -8,5 +8,11 @@ set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 fish_add_path --path "$PNPM_HOME/bin"
 # pnpm end
 
+if test -x /opt/homebrew/bin/brew
+  eval (/opt/homebrew/bin/brew shellenv)
+end
+
+set -gx GOPATH $HOME/go; set -gx GOROOT $HOME/.go; fish_add_path $GOPATH/bin # g-install: do NOT edit, see https://github.com/stefanmaric/g
+
 fish_add_path --move --path "$HOME/.local/bin"
 
